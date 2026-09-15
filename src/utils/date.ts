02d63de3f6b434c2da434da33e15b629c73b3ca4
Konstantin Fedotov <zenflak@gmail.com>
2026-09-15 23:27:04 +0900

    chore(dates): bump date.ts size allowance for sqliteUtcToIso

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // SPDX-FileCopyrightText: 2026 Loop Lore Contributors
 
-// size-allow: 300
+// size-allow: 320
 
 /**
  * Date/time formatting utilities.
